a45882aa7fd6a9a0737fca9b53f33459f88882bf
Nikkk <mailto:cse18ngmishrajd@gmail.com>
2024-05-02 18:54:18 +0530

    LoginDetailsRestController updated

--- src/main/java/com/appxbuild/nagpurit/rest/LoginDetailsRestController.java
+++ src/main/java/com/appxbuild/nagpurit/rest/LoginDetailsRestController.java
@@ -24,7 +24,6 @@ public class LoginDetailsRestController {
         this.loginDetailsService = loginDetailsService;
     }
 
-
     @GetMapping("/loginDetails")
     public List<LoginDetails> findAll(){
         return loginDetailsService.findAll();
