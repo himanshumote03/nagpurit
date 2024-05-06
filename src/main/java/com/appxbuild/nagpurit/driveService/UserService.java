7723d5794df629f7b89778f72d222f878353b1cf
Nikkk <mailto:cse18ngmishrajd@gmail.com>
2024-05-06 15:16:45 +0530

    drive service

---
+++ src/main/java/com/appxbuild/nagpurit/driveService/UserService.java
@@ -0,0 +1,69 @@
+package com.appxbuild.nagpurit.driveService;
+
+import com.appxbuild.nagpurit.entity.User;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+@Service
+public class UserService {
+
+    private static final GsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    private static final String SERVICE_ACCOUNT_KEY_PATH = getPathToGoodleCredentials();
+
+    private static String getPathToGoodleCredentials() {
+        String currentDirectory = System.getProperty("user.dir");
+        Path filePath = Paths.get(currentDirectory, "cred.json");
+        return filePath.toString();
+    }
+    public String uploadImageToDrive(File file) throws GeneralSecurityException, IOException {
+        User user = new User();
+
+        try {
+            // UserImage Folder link
+            String folderId = "1enfia1auN90VWXIIbgaB3Acg-CrKdz5N";
+            Drive drive = createDriveService();
+            com.google.api.services.drive.model.File fileMetaData = new com.google.api.services.drive.model.File();
+            fileMetaData.setName(file.getName());
+            fileMetaData.setParents(Collections.singletonList(folderId));
+            FileContent mediaContent = new FileContent("image/jpeg", file);
+            com.google.api.services.drive.model.File uploadedFile = drive.files().create(fileMetaData, mediaContent)
+                    .setFields("id").execute();
+            String imageUrl = "https://drive.google.com/uc?export=view&id=" + uploadedFile.getId();
+            System.out.println("IMAGE URL: " + imageUrl);
+            file.delete();
+
+            user.setImage(imageUrl);
+            return imageUrl;
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return "Image not found";
+    }
+
+    private Drive createDriveService() throws GeneralSecurityException, IOException {
+
+        GoogleCredential credential = GoogleCredential.fromStream(new FileInputStream(SERVICE_ACCOUNT_KEY_PATH))
+                .createScoped(Collections.singleton(DriveScopes.DRIVE));
+
+        return new Drive.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(),
+                JSON_FACTORY,
+                credential)
+                .build();
+    }
+
+}
